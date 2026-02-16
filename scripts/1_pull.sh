55a7b6747f690dbd92dfa3aac7c653c3c195269c
MrTomatePNG <mrtomate360@outlook.com>
2026-02-16 19:49:22 -0400

    Update 1_pull.sh

--- scripts/1_pull.sh
+++ scripts/1_pull.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Fail fast if any command fails
 set -e
-
+cd /opt/cauldrun
 echo "### 1/5: Puxando código mais recente... ###"
 git fetch origin master
 git reset --hard origin/master
+chmod +x scripts/*.sh
