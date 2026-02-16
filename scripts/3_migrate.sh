47330e116c70aff21072c7f2fcfae72f4910aabe
MrTomatePNG <mrtomate360@outlook.com>
2026-02-16 19:37:54 -0400

    aaaaa

--- scripts/3_migrate.sh
+++ scripts/3_migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Fail fast if any command fails
 set -e
-
+export PATH="/home/cauldrun/.bun/bin:$PATH"
 echo "### 3/5: Rodando as migrations do banco de dados... ###"
 # 'prisma migrate deploy' é o comando seguro para ambientes de produção
 # A DATABASE_URL é esperada como uma variável de ambiente passada pelo runner do CI/CD
--- scripts/5_restart.sh
+++ scripts/5_restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Fail fast if any command fails
 set -e
-
+export PATH="/home/cauldrun/.bun/bin:$PATH"
 echo "### 5/5: Reiniciando o servidor da aplicação... ###"
 pm2 restart sveltekit-app
 pm2 save
