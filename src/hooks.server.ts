78a11fab73520ce5e01f85bf8cab476afd2105af
MrTomatePNG <mrtomate360@outlook.com>
2026-02-19 14:59:01 -0400

    sistema de midleware  e logg

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,25 @@
 import { auth } from "@/lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 import { building } from "$app/environment";
+import { sequence } from "@sveltejs/kit/hooks";
 import type { Handle } from "@sveltejs/kit";
 
-export const handle: Handle = async ({ event, resolve }) => {
+const logger: Handle = async ({ event, resolve }) => {
+  const start = Date.now();
+  const requestMethod = event.request.method;
+  const requestUrl = event.url.pathname;
+
+  const response = await resolve(event);
+
+  const duration = Date.now() - start;
+  console.log(
+    `[${requestMethod}] ${requestUrl} (${response.status}) - ${duration}ms`,
+  );
+
+  return response;
+};
+
+const authHandle: Handle = async ({ event, resolve }) => {
   const session = await auth.api.getSession({
     headers: event.request.headers,
   });
@@ -15,3 +31,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 
   return svelteKitHandler({ event, resolve, auth, building });
 };
+
+export const handle = sequence(logger, authHandle);
