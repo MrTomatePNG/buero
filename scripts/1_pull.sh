3e3c343ad3019d7d112f91435fffe86b26f0da8d
MrTomatePNG <mrtomate360@outlook.com>
2026-02-20 09:48:22 -0400

    me livrando desse lixo

--- scripts/1_pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Fail fast if any command fails
-set -e
-cd /opt/cauldrun
-echo "### 1/5: Puxando código mais recente... ###"
-git fetch origin master
-git reset --hard origin/master
-chmod +x scripts/*.sh
--- scripts/2_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Fail fast if any command fails
-set -e
-export PATH="/home/cauldrun/.bun/bin:$PATH"
-cd /opt/cauldrun
-echo "### 2/5: Instalando dependências... ###"
-# --frozen-lockfile garante que a versão exata do bun.lockb seja instalada
-bun install --frozen-lockfile --bun
