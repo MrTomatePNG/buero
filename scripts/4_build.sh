a85f18fb5a07b9edb0961586dcfb71c47e7d7209
MrTomatePNG <mrtomate360@outlook.com>
2026-02-19 21:13:42 -0400

    Update 4_build.sh

--- scripts/4_build.sh
+++ scripts/4_build.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-# Fail fast if any command fails
-set -e
+# sem set -e
 export PATH="/home/cauldrun/.bun/bin:$PATH"
 cd /opt/cauldrun
 echo "### 4/5: Buildando a aplicação... ###"
-# A DATABASE_URL pode ser necessária durante o build, então o runner do CI/CD deve provê-la
-bun --bun run build
+NODE_OPTIONS="--trace-uncaught --trace-warnings" bun --bun vite build --logLevel silly 2>&1
+echo "Exit code do build: $?"
