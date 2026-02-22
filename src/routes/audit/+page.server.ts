b08d5f243f10639c706a1c2aa3b97b485192b824
MrTomatePNG <mrtomate360@outlook.com>
2026-02-22 17:32:40 -0400

    mismatch path

--- src/routes/audit/+page.server.ts
+++ src/routes/audit/+page.server.ts
@@ -37,7 +37,7 @@ export const load: PageServerLoad = async ({ locals }) => {
         ...post,
         id: post.id.toString(),
         auditUrl,
-      } ;
+      };
     }),
   );
 
@@ -78,7 +78,7 @@ export const actions: Actions = {
         newStatus = "complete";
 
         const oldKey = new URL(post.mediaUrl).pathname.slice(1);
-        const newKey = oldKey.replace("/uploads/pending/", "/uploads/public");
+        const newKey = oldKey.replace("uploads/pending/", "uploads/public");
 
         await s3Client.send(
           new CopyObjectCommand({
